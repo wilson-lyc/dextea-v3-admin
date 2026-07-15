ef7cd6678c911ea91687266a6b8f960407b70c60
wilson-lyc <wilson_lyc@foxmail.com>
2026-07-15 15:47:38 +0800

    revert(db): 移除 product_images 表迁移

--- apps/api/src/plugins/db/mysql/0001_demonic_brood.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `product_images` (
-	`product_id` bigint unsigned NOT NULL,
-	`image_id` bigint unsigned NOT NULL,
-	`type` tinyint NOT NULL,
-	`sort` int NOT NULL DEFAULT 0,
-	`created_at` timestamp NOT NULL DEFAULT (now()),
-	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT `pk_product_images` PRIMARY KEY(`product_id`,`image_id`,`type`)
-);
