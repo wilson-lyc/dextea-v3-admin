65d40c91919baed21cb9410eaa8fec438ac53f75
wilson-lyc <wilson_lyc@foxmail.com>
2026-07-15 14:31:59 +0800

    style(modal): 调整权限配置对话框尺寸

--- apps/web/src/pages/Roles/components/AssignPermissionsModal.tsx
+++ apps/web/src/pages/Roles/components/AssignPermissionsModal.tsx
@@ -116,7 +116,7 @@ export default function AssignPermissionsModal({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="flex w-[20vw] h-[40vh] flex-col">
+      <DialogContent className="flex w-[40vw] h-[60vh] flex-col">
         <DialogHeader>
           <DialogTitle>权限配置</DialogTitle>
         </DialogHeader>
